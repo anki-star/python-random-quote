f1733d777b776e8b9e1142874939cc9aea9debeb
anki-star <ankitsaini805496@gmail.com>
2020-09-18 16:16:12 +0530

    random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
 def primary():
-   print("Keep it logically awesome.")
+   #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = len(quotes) -1
+  why = random.randint(0, last)
+  print(quotes[why])
 
 if __name__== "__main__":
   primary()
