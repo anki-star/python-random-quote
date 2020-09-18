b0b7c7b19b6284c62eae1884b766de09ca6ed3a5
anki-star <ankitsaini805496@gmail.com>
2020-09-18 16:34:53 +0530

    yo bro

--- get-quote.py
+++ get-quote.py
@@ -7,7 +7,9 @@ def primary():
   f.close()
   last = len(quotes) -1
   why = random.randint(0, last)
-  print(quotes[why])
-
+  #print(quotes[why])
+  print(quotes[15])
+  print(quotes[14])
+  print(quotes[13])
 if __name__== "__main__":
   primary()
